a494148ac3bcecaef4e65ca25f3c301baa8eb515
David <davidkarpenko80@gmail.com>
2023-09-07 10:59:13 +0300

    Add display word function

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import random
 def choose_word():
     words = ['apple', 'peach', 'strawberry']
     return random.choice(words)
+
+
+def display_word(word, guessed_letters):
+    display = ''
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += '*'
+    return display
+
