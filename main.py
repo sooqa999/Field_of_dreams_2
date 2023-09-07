8517bc0371074fea640858329e23e3d39ae5265b
David <davidkarpenko80@gmail.com>
2023-09-07 11:00:07 +0300

    Add main function

--- main.py
+++ main.py
@@ -15,3 +15,32 @@ def display_word(word, guessed_letters):
             display += '*'
     return display
 
+
+def main():
+    word_to_guess = choose_word()
+    guessed_letters = []
+    attempts = int(input('Enter number of attempts: '))
+
+    print("Welcome to field of dreams!")
+
+    while True:
+        print('\nСлово:', display_word(word_to_guess, guessed_letters))
+        guess = input("Enter letter: ").lower()
+
+        if guess in guessed_letters:
+            print('You already guessed this letter')
+        elif guess in word_to_guess:
+            guessed_letters.append(guess)
+            print(guess)
+        else:
+            attempts -= 1
+            print('Failed')
+
+        if '*' not in display_word(word_to_guess, guessed_letters):
+            print('\nCongratulations! You win!')
+            break
+
+        if attempts == 0:
+            print("Game over. Guessed word: ", word_to_guess)
+            break
+
